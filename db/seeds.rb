79fd67c3e1044c1d08d5ed49afbf3bb281b6d8a3
Vanessa Loviton <loviton.vanessa@yahoo.fr>
2017-05-18 17:11:22 +0200

    Styling hard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Ingredient.destroy_all
+Cocktail.destroy_all
 
 puts "Opening seed file..."
 responses = RestClient.get "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
@@ -11,9 +12,9 @@ response["drinks"].each do |ingredient|
 end
 
 puts "Creating Cocktails seeds"
-Cocktail.new(name:"Pina Colada")
-Cocktail.new(name:"Bloody Mary")
-Cocktail.new(name:"Cuba Libre")
-Cocktail.new(name:"Sex on the Beach")
+Cocktail.create!(name:"Pina Colada", image:"http://www.sofizz.fr/blog/wp-content/uploads/2015/10/pinacolada.jpg")
+Cocktail.create!(name:"Bloody Mary", image:"https://www.bulleit.com/images/background/out/2048/bourbon-bloody-mary-hero.jpg")
+Cocktail.create!(name:"Cuba Libre", image:"http://www.distilleurs.fr/wp-content/uploads/2015/08/fc-rc-step0-bg.jpg")
+Cocktail.create!(name:"Sex on the Beach", image:"http://www.absolutdrinks.com/wp-content/themes/drinks_v2/img/custom_card_images/sex-on-the-beach.jpg")
 
 puts "Seeding complete"
